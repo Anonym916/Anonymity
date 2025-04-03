3fb1d318356b15beb4e2812f71f3dd00659bb67c
Anonym916 <fangz2023@163.com>
2025-04-03 01:21:01 +0100

    Update costloss.py

--- module/costloss.py
+++ module/costloss.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+from src.utils import mul
 
 
 def flatten(lst):
@@ -60,16 +61,16 @@ def costloss(mask):
     block_unet = np.array([47185920.0, 15992340480.0, 943718400.0, 15992340480.0, 14345011200.0, 943718400.0, 16022732800.0, 14417592320.0, 943718400.0, 16095313920.0, 1889075200.0, 1889075200.0, 6027673600.0, 1889075200.0, 1889075200.0, 1889075200.0, 3774873600.0, 16095313920.0, 16095313920.0, 16095313920.0, 15099494400.0, 20636467200.0, 16022732800.0, 16022732800.0, 15099494400.0, 20606074880.0, 15992340480.0, 15992340480.0])
 
     # (layers, bs, mask) -> (bs, layers, mask)
-    # mask = convert_list_to_tensor(mask, dim=0)
-    # mask_bs = mask.permute(1, 0, 2)
-    mask_bs = np.array(mask)
-    mask_bs = mask_bs.transpose((1, 0, 2))
+    mask = convert_list_to_tensor(mask, dim=0)
+    mask_bs = mask.permute(1, 0, 2)
+    # mask_bs = np.array(mask)
+    # mask_bs = mask_bs.transpose((1, 0, 2))
 
     ratio_bs = []
     for mask in mask_bs:
         mask = mask.flatten()
         # ratio = torch.sum(mask>0).item() / len(mask)
-        flops = mask * block_unet
+        flops = mul(mask, block_unet)
         ratio = flops.sum() / flops_unet
         ratio_bs.append(ratio)
 
